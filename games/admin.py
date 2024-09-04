ca3a19bcb64c2f432ae1fffadf7ce1fafb39dbb3
RomanKuschanow <kuschanow@gmail.com>
2024-09-04 18:09:07 +0300

    add models to admin panel

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from games.models import *
+
 # Register your models here.
+admin.site.register(Punishment)
+admin.site.register(RandomChoiceGame)
+admin.site.register(RandomChoiceGamePlayer)
+admin.site.register(RandomChoiceGameResult)
+admin.site.register(RandomChoiceGameLoser)
