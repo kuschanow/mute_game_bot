873179f89be3199ea711a762bff2d4f67d5f2333
RomanKuschanow <kuschanow@gmail.com>
2024-09-04 22:01:32 +0300

    add logging in run_bot

--- bot/management/commands/run_bot.py
+++ bot/management/commands/run_bot.py
@@ -70,5 +70,8 @@ class Command(BaseCommand):
             web.run_app(app, host=settings.WEB_SERVER_HOST, port=settings.WEB_SERVER_PORT)
 
             asyncio.run(bot.set_webhook(f"{settings.BASE_WEBHOOK_URL}{settings.WEBHOOK_PATH}", secret_token=settings.WEBHOOK_SECRET))
+
+            logger.info("Run via webhook")
         else:
             asyncio.run(dp.start_polling(bot))
+            logger.info("Run via polling")
